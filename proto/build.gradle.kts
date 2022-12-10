2e0c20092358996fe4e7ecfa92fe664b1cb9ea59
Manoo Srivastav <manoo.srivastav@gmail.com>
2022-12-10 17:45:40 +0000

    Fix proto, and convert identity to kotlin

--- proto/build.gradle.kts
+++ proto/build.gradle.kts
@@ -14,15 +14,9 @@ repositories {
 
 dependencies {
     implementation("com.google.protobuf:protobuf-java:3.21.9")
-    implementation("io.grpc:grpc-stub:1.50.2")
-    implementation("io.grpc:grpc-protobuf:1.50.2")
+    implementation("io.grpc:grpc-stub:1.51.0")
+    implementation("io.grpc:grpc-protobuf:1.51.0")
     implementation("javax.annotation:javax.annotation-api:1.3.2")
-    implementation("io.github.lognet:grpc-spring-boot-starter:4.9.1")
-    implementation("me.dinowernli:java-grpc-prometheus:0.3.0")
-
-
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
 }
 
 tasks.getByName<Test>("test") {
